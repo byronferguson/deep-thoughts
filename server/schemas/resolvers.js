7851047a0bf1ba134166b0b1778291ee1df7e324
Byron Ferguson <ferguson.bs@gmail.com>
2020-10-05 17:33:35 -0500

    JWT and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { Thought, User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
@@ -9,6 +11,19 @@ const resolvers = {
 
     thought: async (_, { _id }) => {
       return Thought.findOne({ _id });
+    },
+
+    me: async (_, _args, ctx) => {
+      if (!ctx.user) {
+        throw new AuthenticationError('Not logged in');
+      }
+
+      const userData = await User.findById(ctx.user._id)
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
+
+      return userData;
     },
 
     users: async () => {
@@ -23,6 +38,80 @@ const resolvers = {
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
+    },
+  },
+
+  Mutation: {
+    addUser: async (_, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { user, token };
+    },
+
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { user, token };
+    },
+
+    addThought: async (_, args, ctx) => {
+      if (!ctx.user) {
+        throw new AuthenticationError('You need to be logged in!');
+      }
+
+      const thought = await Thought.create({
+        ...args,
+        username: ctx.user.username,
+      });
+
+      await User.findByIdAndUpdate(
+        { _id: ctx.user._id },
+        { $push: { thoughts: thought._id } },
+        { new: true }
+      );
+
+      return thought;
+    },
+
+    addReaction: async (_, { thoughtId, reactionBody }, ctx) => {
+      if (!ctx.user) {
+        throw new AuthenticationError('You need to be logged in!');
+      }
+
+      const updatedThought = await Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        { $push: { reactions: { reactionBody, username: ctx.user.username } } },
+        { new: true, runValidators: true }
+      );
+
+      return updatedThought;
+    },
+
+    addFriend: async (_, { friendId }, ctx) => {
+      if (!ctx.user) {
+        throw new AuthenticationError('You need to be logged in!');
+      }
+
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: ctx.user._id },
+        { $addToSet: { friends: friendId } },
+        { new: true }
+      ).populate('friends');
+
+      return updatedUser;
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,6 +17,11 @@ const typeDefs = gql`
     username: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type User {
     _id: ID
     username: String
@@ -27,10 +32,19 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
