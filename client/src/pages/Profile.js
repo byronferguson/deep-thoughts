43a564b864aa25cdc0c565a2142107a481752a69
Byron Ferguson <ferguson.bs@gmail.com>
2020-10-06 10:00:04 -0500

    connecting Profile, adding FriendList

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_USER } from '../utils/queries';
+
+import FriendList from '../components/FriendList';
+import ThoughtList from '../components/ThoughtList';
 
 const Profile = () => {
-  return (
-    <div>
-      <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          {/* Viewing <usernames>'s profile. */}
-        </h2>
+  const { username } = useParams();
+  const { data, loading } = useQuery(QUERY_USER, {
+    variables: { username },
+  });
+
+  const user = data?.user ?? {};
+
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
+    <div className="flex-row justify-space-between mb-3">
+      <div className="col-12 mb-3 col-lg-8">
+        <ThoughtList
+          thoughts={user.thoughts}
+          title={`${user.username}'s thoughts...`}
+        />
       </div>
 
-      <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">{/* PRINT THOUGHT LIST  */}</div>
-
-        <div className="col-12 col-lg-3 mb-3">{/* PRINT FRIEND LIST */}</div>
+      <div className="col-12 col-lg-3 mb-3">
+        <FriendList
+          username={user.username}
+          friendCount={user.friendCount}
+          friends={user.friends}
+        />
       </div>
     </div>
   );
