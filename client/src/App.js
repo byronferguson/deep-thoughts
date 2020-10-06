9b7e491292b3b4eeea1bbe50e723c14f5ec5b070
Byron Ferguson <ferguson.bs@gmail.com>
2020-10-06 10:49:51 -0500

    persist JWT

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,16 @@ import Signup from './pages/Signup';
 
 const client = new ApolloClient({
   uri: '/graphql',
+
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
 });
 
 function App() {
